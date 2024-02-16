a791a3d47a6436422bd7954e20ee4afeceb7ae30
Matheus Santos <80715832+matheus412@users.noreply.github.com>
2024-02-15 22:18:10 -0300

    nav bar

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
+///menu burguer
+document.getElementById('burguer').addEventListener('click', function() {
+    var menu = document.getElementById('itens');
+    
+    if (menu.classList.contains('active')) {
+      menu.classList.remove('active');
+    } else {
+      menu.classList.add('active');
+    }
+  });
+///imagens
+
 document.addEventListener("DOMContentLoaded", function() {
     var imagens = document.querySelectorAll('#fotos-prato img');
     var observer = new IntersectionObserver(function(entries) {
